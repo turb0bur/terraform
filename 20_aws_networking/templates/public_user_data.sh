b9496cf6b820ac0fa595f8ece09eeb91ee711223
Volodymyr Butko <turb0bur@ukr.net>
2024-09-18 16:33:44 +0300

    EPAM-DevOps Refactored public_user_data.sh

--- 20_aws_networking/templates/public_user_data.sh
+++ 20_aws_networking/templates/public_user_data.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-sudo su
-yum update
-yum install -y nginx
+function install_web_server() {
+    yum update
+    yum install -y nginx
+}
 
-TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
-IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --header "X-aws-ec2-metadata-token: $TOKEN")
-HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname --header "X-aws-ec2-metadata-token: $TOKEN")
+function get_instance_metadata() {
+    TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
+    IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --header "X-aws-ec2-metadata-token: $TOKEN")
+    HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname --header "X-aws-ec2-metadata-token: $TOKEN")
+}
 
-cat <<EOF > /usr/share/nginx/html/index.html
+function set_web_server_index_page_content() {
+    cat <<EOF > /usr/share/nginx/html/index.html
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,6 +25,18 @@ cat <<EOF > /usr/share/nginx/html/index.html
 </body>
 </html>
 EOF
+}
 
-systemctl start nginx
-systemctl enable nginx
+function start_and_enable_web_server() {
+    systemctl start nginx
+    systemctl enable nginx
+}
+
+function main() {
+    install_web_server
+    get_instance_metadata
+    set_web_server_index_page_content
+    start_and_enable_web_server
+}
+
+main
