dea1f471d0c6017bded4eadbcdd5b8345d8915c9
Volodymyr Butko <turb0bur@ukr.net>
2024-09-24 16:35:42 +0300

    EPAM-DevOps Added ECS needed IAM permissions and added policy to modify EC2 attributes for existing NAT instance IAM role/profile

--- 20_aws_networking/aws_iam.tf
+++ 20_aws_networking/aws_iam.tf
@@ -1,5 +1,8 @@
-resource "aws_iam_role" "ec2_role" {
-  name = format(local.resource_name, "ec2-role")
+##########################################################################################
+# IAM instance role for Petlinic application EC2 instances
+##########################################################################################
+resource "aws_iam_role" "petclinic_ec2" {
+  name = format(local.resource_name, "petclinic-ec2-role")
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -14,19 +17,53 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_role_policy" {
-  role       = aws_iam_role.ec2_role.name
+resource "aws_iam_role_policy_attachment" "petclinic_ssm_role_policy" {
+  role       = aws_iam_role.petclinic_ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = format(local.resource_name, "ssm-instance-profile")
-  role = aws_iam_role.ec2_role.name
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.petclinic_ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "petclinic_profile" {
+  name = format(local.resource_name, "petclinic-profile")
+  role = aws_iam_role.petclinic_ec2.name
+}
+
+##########################################################################################
+# IAM role for NAT EC2 instance
+##########################################################################################
+resource "aws_iam_role" "nat_ec2" {
+  name = format(local.resource_name, "nat-ec2-role")
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "nat_profile" {
+  name = format(local.resource_name, "nat-profile")
+  role = aws_iam_role.nat_ec2.name
+}
+
+resource "aws_iam_role_policy_attachment" "nat_ssm_role_policy" {
+  role       = aws_iam_role.nat_ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy" "ec2_create_route" {
   name = format(local.resource_name, "ec2-create-route-policy")
-  role = aws_iam_role.ec2_role.id
+  role = aws_iam_role.nat_ec2.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -37,4 +74,47 @@ resource "aws_iam_role_policy" "ec2_create_route" {
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy" "modify_instance_attribute" {
+  name = format(local.resource_name, "modify-instance-attribute-policy")
+  role = aws_iam_role.nat_ec2.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "ec2:ModifyInstanceAttribute",
+        Resource = format("arn:aws:ec2:%s:%s:instance/*", var.region, data.aws_caller_identity.current.account_id)
+      }
+    ]
+  })
+}
+
+##########################################################################################
+# IAM role grants ECS tasks permission to pull images from ECR and send logs to CloudWatch
+##########################################################################################
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = format(local.resource_name, "ecs-task-execution-role")
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = format(local.resource_name, "ecs-task-execution-role")
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
